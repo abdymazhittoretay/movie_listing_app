4fefd51f72081d55a9ee209a0af49828275d02b7
Toretay <abdymvzhit@gmail.com>
2025-06-26 12:49:09 +0500

    decided not to use provider for now

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:movie_listing_app/providers/movie_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:movie_listing_app/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,12 +9,7 @@ void main() async {
   } catch (e) {
     throw Exception('Error loading .env file: $e');
   }
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => MovieProvider(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -23,6 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
