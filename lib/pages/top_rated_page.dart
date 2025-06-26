5e332537615590a28cf3c5f3509fd2345797757c
Toretay <abdymvzhit@gmail.com>
2025-06-26 14:02:07 +0500

    created top_rated_page

---
+++ lib/pages/top_rated_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:movie_listing_app/models/movie_model.dart';
+import 'package:movie_listing_app/services/api_service.dart';
+
+class TopRatedPage extends StatefulWidget {
+  const TopRatedPage({super.key});
+
+  @override
+  State<TopRatedPage> createState() => _TopRatedPageState();
+}
+
+class _TopRatedPageState extends State<TopRatedPage> {
+  late Future<List<MovieModel>> topRatedMovies;
+
+  @override
+  void initState() {
+    super.initState();
+    topRatedMovies = apiService.value.getTopRatedMovies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Top Rated Movies')),
+      body: FutureBuilder<List<MovieModel>>(
+        future: topRatedMovies,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text("${snapshot.error}"));
+          }
+
+          if (!snapshot.hasData || snapshot.data == null) {
+            return const Center(child: Text("No data received."));
+          }
+
+          final List<MovieModel> movies = snapshot.data!;
+
+          if (movies.isEmpty) {
+            return const Center(child: Text("Movie list is empty!"));
+          }
+
+          return ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final MovieModel movie = movies[index];
+              return ListTile(
+                title: Text(movie.title),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _getRatingIcon(movie.rating),
+                    Text("${movie.rating.toStringAsFixed(1)} - TMDb"),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Icon _getRatingIcon(double rating) {
+    if (rating >= 8.0) {
+      return const Icon(Icons.star, color: Colors.green);
+    } else if (rating >= 5.0) {
+      return const Icon(Icons.star_half, color: Colors.amber);
+    } else {
+      return const Icon(Icons.star_border, color: Colors.red);
+    }
+  }
+}
