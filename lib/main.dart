35e091f98e9a6bfa9754a8d5801b18f0cfee62a2
Toretay <abdymvzhit@gmail.com>
2025-06-26 12:23:58 +0500

    added provider in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movie_listing_app/providers/movie_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,7 +10,12 @@ void main() async {
   } catch (e) {
     throw Exception('Error loading .env file: $e');
   }
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => MovieProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
