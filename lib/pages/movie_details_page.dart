95510fb73a248fad4c57c2cb6bb6f163d5a8d38d
Toretay <abdymvzhit@gmail.com>
2025-06-27 15:49:57 +0500

    changed ui/ux of movie details

--- lib/pages/movie_details_page.dart
+++ lib/pages/movie_details_page.dart
@@ -10,34 +10,79 @@ class MovieDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(movie.title)),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(child: Image.network(movie.fullPosterUrl, width: 200)),
-            const SizedBox(height: 16),
-            Text(
-              movie.title,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            Row(
-              children: [
-                _getRatingIcon(movie.rating),
-                const SizedBox(width: 8),
-                Text(
-                  '${movie.rating.toStringAsFixed(1)} / 10 - TMDb',
-                  style: const TextStyle(fontSize: 16),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 64.0,
+            top: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.network(
+                  movie.fullPosterUrl,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) return child;
+
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes!
+                            : null,
+                      ),
+                    );
+                  },
+                  errorBuilder: (context, error, stackTrace) =>
+                      const Icon(Icons.broken_image, size: 100),
                 ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Text(
-              movie.overview.isNotEmpty ? movie.overview : "No description.",
-              style: const TextStyle(fontSize: 16),
-            ),
-          ],
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                movie.title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _getRatingIcon(movie.rating),
+                  const SizedBox(width: 8),
+                  Text(
+                    '${movie.rating.toStringAsFixed(1)} / 10 - TMDb',
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+              const Divider(height: 32.0, thickness: 1.3, color: Colors.black),
+              Row(
+                children: [
+                  Text(
+                    "Description:",
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                textAlign: TextAlign.justify,
+                movie.overview.isNotEmpty ? movie.overview : "No description.",
+                style: const TextStyle(fontSize: 18.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
