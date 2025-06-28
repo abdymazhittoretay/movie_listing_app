5f05a8a12898e892c232a46bd513a9cbfaa3d845
Toretay <abdymvzhit@gmail.com>
2025-06-28 18:58:26 +0500

    created method to toggleFavorite

--- lib/pages/movie_details_page.dart
+++ lib/pages/movie_details_page.dart
@@ -1,12 +1,59 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:movie_listing_app/models/movie_model.dart';
 import 'package:movie_listing_app/utils/rating_utils.dart';
 
-class MovieDetailPage extends StatelessWidget {
+class MovieDetailPage extends StatefulWidget {
   final MovieModel movie;
 
   const MovieDetailPage({super.key, required this.movie});
+
+  @override
+  State<MovieDetailPage> createState() => _MovieDetailPageState();
+}
+
+class _MovieDetailPageState extends State<MovieDetailPage> {
+  late Box<MovieModel> _favoritesBox;
+  bool _isFavorite = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _favoritesBox = Hive.box<MovieModel>('favoritesBox');
+    _isFavorite = _favoritesBox.containsKey(widget.movie.id);
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorite) {
+        _favoritesBox.delete(widget.movie.id);
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            const SnackBar(
+              backgroundColor: Colors.black87,
+              content: Center(
+                child: Text("Removed from Watch Later", style: TextStyle()),
+              ),
+              duration: Duration(seconds: 1),
+            ),
+          );
+      } else {
+        _favoritesBox.put(widget.movie.id, widget.movie);
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            const SnackBar(
+              backgroundColor: Colors.black87,
+              content: Center(child: Text("Added to Watch Later")),
+              duration: Duration(seconds: 1),
+            ),
+          );
+      }
+      _isFavorite = !_isFavorite;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +67,9 @@ class MovieDetailPage extends StatelessWidget {
           children: [
             const Icon(Icons.movie),
             const SizedBox(width: 8.0),
-            Flexible(child: Text(movie.title, overflow: TextOverflow.ellipsis)),
+            Flexible(
+              child: Text(widget.movie.title, overflow: TextOverflow.ellipsis),
+            ),
           ],
         ),
         centerTitle: true,
@@ -35,29 +84,49 @@ class MovieDetailPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Center(
-                    child: CachedNetworkImage(
-                      imageUrl: movie.fullPosterUrl,
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) => const SizedBox(
-                        width: 150,
-                        height: 220,
-                        child: Center(
-                          child: CircularProgressIndicator(color: Colors.black),
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        CachedNetworkImage(
+                          imageUrl: widget.movie.fullPosterUrl,
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) => const SizedBox(
+                            width: 150,
+                            height: 220,
+                            child: Center(
+                              child: CircularProgressIndicator(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          errorWidget: (context, url, error) => const SizedBox(
+                            width: 150,
+                            height: 220,
+                            child: Icon(
+                              Icons.image_not_supported_outlined,
+                              size: 80,
+                            ),
+                          ),
                         ),
-                      ),
-                      errorWidget: (context, url, error) => const SizedBox(
-                        width: 150,
-                        height: 220,
-                        child: Icon(
-                          Icons.image_not_supported_outlined,
-                          size: 80,
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: IconButton(
+                            onPressed: _toggleFavorite,
+                            icon: Icon(
+                              _isFavorite
+                                  ? Icons.bookmark
+                                  : Icons.bookmark_add_outlined,
+                              color: Colors.white,
+                              size: 40.0,
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
                   ),
                   const SizedBox(height: 16.0),
                   Text(
-                    "${movie.title}${movie.releaseDate.isNotEmpty ? ", ${movie.releaseDate.substring(0, 4)}" : ""}",
+                    "${widget.movie.title}${widget.movie.releaseDate.isNotEmpty ? ", ${widget.movie.releaseDate.substring(0, 4)}" : ""}",
                     textAlign: TextAlign.center,
                     style: const TextStyle(
                       fontSize: 24,
@@ -69,10 +138,10 @@ class MovieDetailPage extends StatelessWidget {
                     mainAxisSize: MainAxisSize.min,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      getRatingIcon(movie.rating),
+                      getRatingIcon(widget.movie.rating),
                       const SizedBox(width: 8),
                       Text(
-                        '${movie.rating.toStringAsFixed(1)} / 10 - TMDb',
+                        '${widget.movie.rating.toStringAsFixed(1)} / 10 - TMDb',
                         style: const TextStyle(fontSize: 16),
                       ),
                     ],
@@ -94,8 +163,8 @@ class MovieDetailPage extends StatelessWidget {
                   ),
                   const SizedBox(height: 8.0),
                   Text(
-                    movie.overview.isNotEmpty
-                        ? movie.overview
+                    widget.movie.overview.isNotEmpty
+                        ? widget.movie.overview
                         : "No description.",
                     textAlign: TextAlign.justify,
                     style: const TextStyle(fontSize: 18.0),
